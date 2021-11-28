9e2604b78a770889eeced5b0ef48cd1c498a6aa7
Ryavell <valeriy223@yandex.ru>
2021-11-28 20:37:35 +0300

    Добавьте файлы проекта.

---
+++ VisualStudioDiscordRPC.Shared/DteController.cs
@@ -0,0 +1,39 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+
+namespace VisualStudioDiscordRPC
+{
+    internal class DteController : IDisposable
+    {
+        DTE _instance;
+
+        public DteController()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            _instance = (DTE)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+
+            if (_instance == null)
+            {
+                throw new InvalidOperationException("Can not get DTE Service");
+            }
+
+            _instance.Events.WindowEvents.WindowActivated += WindowEvents_WindowActivated;
+        }
+
+        private void WindowEvents_WindowActivated(Window GotFocus, Window LostFocus)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            System.Windows.MessageBox.Show($"Caption: {GotFocus.Caption}\nType: {GotFocus?.Type}");
+        }
+
+        public void Dispose()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            _instance.Events.WindowEvents.WindowActivated -= WindowEvents_WindowActivated;
+        }
+    }
+}
