113c092b00a50fa4ad56d61d1680576cc1bda4d1
Ryavell <valeriy223@yandex.ru>
2021-11-29 01:25:16 +0300

    Added template of rich presence

--- VisualStudioDiscordRPC.Shared/DteController.cs
+++ VisualStudioDiscordRPC.Shared/DteController.cs
@@ -1,26 +1,45 @@
-﻿using EnvDTE;
+﻿using DiscordRPC;
+using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using System;
-using System.Diagnostics;
 
 namespace VisualStudioDiscordRPC
 {
     internal class DteController : IDisposable
     {
         DTE _instance;
+        DiscordRpcClient _client;
 
         public DteController()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
+            // DTE settings
             _instance = (DTE)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
-
+            
             if (_instance == null)
             {
                 throw new InvalidOperationException("Can not get DTE Service");
             }
 
             _instance.Events.WindowEvents.WindowActivated += WindowEvents_WindowActivated;
+
+            // Discord Rich Presense client settings
+            _client = new DiscordRpcClient("914622396630175855");
+
+            _client.Initialize();
+
+            _client.SetPresence(new RichPresence()
+            {
+                Details = "Example Project",
+                State = "csharp example",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "image_large",
+                    LargeImageText = "Lachee's Discord IPC Library",
+                    SmallImageKey = "image_small"
+                }
+            });
         }
 
         private void WindowEvents_WindowActivated(Window GotFocus, Window LostFocus)
@@ -35,6 +54,7 @@ namespace VisualStudioDiscordRPC
             ThreadHelper.ThrowIfNotOnUIThread();
 
             _instance.Events.WindowEvents.WindowActivated -= WindowEvents_WindowActivated;
+            _client.Dispose();
         }
     }
 }
