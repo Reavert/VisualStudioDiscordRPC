61a4ee859762c9059d791aec6b8077062eef4d8b
Ryavell <valeriy223@yandex.ru>
2021-12-21 19:05:43 +0300

    Added commentaries for constant string formats

--- VisualStudioDiscordRPC.Shared/ConstantStrings.cs
+++ VisualStudioDiscordRPC.Shared/ConstantStrings.cs
@@ -2,10 +2,26 @@
 {
     public static class ConstantStrings
     {
+        /// <summary>
+        /// {0} - localized word of "File"<br/>
+        /// {1} - file name
+        /// </summary>
         public const string ActiveFileFormat = "{0} {1}";
+
+        /// <summary>
+        /// {0} - localized word of "Project"<br/>
+        /// {1} - project name
+        /// </summary>
         public const string ActiveProjectFormat = "{0} {1}";
 
+        /// <summary>
+        /// {0} - Visual Studio version
+        /// </summary>
         public const string VisualStudioVersion = "Visual Studio {0}";
+
+        /// <summary>
+        /// {0} - Visual Studio version
+        /// </summary>
         public const string VisualStudioVersionAssetKey = "vs_{0}";
     }
 }
