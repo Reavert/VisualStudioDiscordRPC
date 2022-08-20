98d42732d7f2bb6e12e1224aa71a5573d4050284
Ryavell <valeriy223@yandex.ru>
2022-08-21 01:13:59 +0300

    Added filename slot

---
+++ VisualStudioDiscordRPC.Shared/Slots/FileNameSlot.cs
@@ -0,0 +1,47 @@
+﻿using EnvDTE;
+using System;
+using VisualStudioDiscordRPC.Shared.Localization;
+using VisualStudioDiscordRPC.Shared.Localization.Models;
+using VisualStudioDiscordRPC.Shared.Observers;
+using VisualStudioDiscordRPC.Shared.Services.Models;
+
+namespace VisualStudioDiscordRPC.Shared.Slots
+{
+    public class FileNameSlot : AbstractSlot
+    {
+        private LocalizationService<LocalizationFile> _localizationService;
+
+        public FileNameSlot(IObserver observer) : base(observer)
+        {
+            _localizationService = ServiceRepository.Default.GetService<LocalizationService<LocalizationFile>>();
+        }
+
+        public override void Enable()
+        {
+            Observer.DocumentChanged += OnDocumentChanged;
+        }
+
+        public override void Disable()
+        {
+            Observer.DocumentChanged -= OnDocumentChanged;
+        }
+
+        private void OnDocumentChanged(Document document)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            string filenameText = string.Empty;
+            if (document != null)
+            {
+                filenameText =
+                    string.Format(ConstantStrings.ActiveFileFormat, _localizationService.Current.File, document.Name);
+            }
+            else
+            {
+                filenameText = _localizationService.Current.NoActiveFile;
+            }
+            
+            PerformUpdate(filenameText);
+        }
+    }
+}
