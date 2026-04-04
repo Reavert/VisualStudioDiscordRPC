a7e6b9acf8e06340f1ed509e6ff83626571e804e
Reavert <valeriy223@yandex.ru>
2026-04-04 17:40:15 +0300

    Git repository button disabled by default to protect against accidental leakage of repository

--- VisualStudioDiscordRPC.Shared/Services/SettingsService.cs
+++ VisualStudioDiscordRPC.Shared/Services/SettingsService.cs
@@ -30,7 +30,7 @@ namespace VisualStudioDiscordRPC.Shared.Services
 
             if (!File.Exists(SettingsPath))
             {
-                MigrateFromLegacySettings();
+                PopulateDefaultProperties();
                 Save();
             }
             else
@@ -38,7 +38,6 @@ namespace VisualStudioDiscordRPC.Shared.Services
                 string json = File.ReadAllText(SettingsPath);
                 _settingsMap = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
             }
-
         }
 
         public T Read<T>(string key, T defaultValue = default)
@@ -69,46 +68,13 @@ namespace VisualStudioDiscordRPC.Shared.Services
             _settingsMap[SettingsKeys.DetailsPlug] = typeof(FileNameTextPlug).Name;
             _settingsMap[SettingsKeys.StatePlug] = typeof(SolutionNameTextPlug).Name;
             _settingsMap[SettingsKeys.TimerPlug] = typeof(FileScopeTimerPlug).Name;
-            _settingsMap[SettingsKeys.FirstButtonPlug] = typeof(GitRepositoryButtonPlug).Name;
+            _settingsMap[SettingsKeys.FirstButtonPlug] = typeof(NoneButtonPlug).Name;
             _settingsMap[SettingsKeys.SecondButtonPlug] = typeof(NoneButtonPlug).Name;
             _settingsMap[SettingsKeys.ApplicationID] = DiscordRpcController.DefaultApplicationId;
             _settingsMap[SettingsKeys.UpdateTimeout] = (long) 1000;
             _settingsMap[SettingsKeys.Version] = "1.0.0";
             _settingsMap[SettingsKeys.UpdateNotifications] = true;
             _settingsMap[SettingsKeys.TranslationsPath] = "Translations/";
-        }
-
-        private void MigrateFromLegacySettings()
-        {
-            _settingsMap[SettingsKeys.RichPresenceEnabled] = bool.Parse(Settings.Default.RichPresenceEnabled);
-            _settingsMap[SettingsKeys.Language] = Settings.Default.Language;
-
-            _settingsMap[SettingsKeys.LargeIconPlug] = 
-                MigrationHelper.GetAssetPlugNameFromLegacy(Settings.Default.LargeIconSlot, nameof(ExtensionIconPlug));
-
-            _settingsMap[SettingsKeys.SmallIconPlug] =
-                MigrationHelper.GetAssetPlugNameFromLegacy(Settings.Default.SmallIconSlot, nameof(VisualStudioVersionIconPlug));
-
-            _settingsMap[SettingsKeys.DetailsPlug] =
-                MigrationHelper.GetTextPlugNameFromLegacy(Settings.Default.DetailsSlot, nameof(FileNameTextPlug));
-
-            _settingsMap[SettingsKeys.StatePlug] =
-                MigrationHelper.GetTextPlugNameFromLegacy(Settings.Default.StateSlot, nameof(SolutionNameTextPlug));
-
-            _settingsMap[SettingsKeys.TimerPlug] =
-                MigrationHelper.GetTimerPlugNameFromLegacy(Settings.Default.TimerSlot, nameof(FileScopeTimerPlug));
-
-            _settingsMap[SettingsKeys.FirstButtonPlug] =
-                MigrationHelper.GetButtonPlugNameFromLegacy(Settings.Default.FirstButtonSlot, nameof(GitRepositoryButtonPlug));
-
-            _settingsMap[SettingsKeys.SecondButtonPlug] =
-                MigrationHelper.GetButtonPlugNameFromLegacy(Settings.Default.SecondButtonSlot, nameof(NoneButtonPlug));
-
-            _settingsMap[SettingsKeys.ApplicationID] = Settings.Default.ApplicationID;
-            _settingsMap[SettingsKeys.UpdateTimeout] = long.Parse(Settings.Default.UpdateTimeout);
-            _settingsMap[SettingsKeys.Version] = Settings.Default.Version;
-            _settingsMap[SettingsKeys.UpdateNotifications] = bool.Parse(Settings.Default.UpdateNotifications);
-            _settingsMap[SettingsKeys.TranslationsPath] = Settings.Default.TranslationsPath;
         }
     }
 }
